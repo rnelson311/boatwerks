649a9898fa050ad894a5f12d9e5a8d3d4ddcd9b7
Robert Nelson <nelson.robert.edward@gmail.com>
2018-06-09 09:48:08 -0500

    green specs

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,17 @@
 class Client < ApplicationRecord
+
+  before_save :downcase_email
+
+  validates :first_name,  presence: true, length: { maximum: 50 }
+  validates :last_name,  presence: true, length: { maximum: 50 }
+  validates :phone,  presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+
+  private
+  def downcase_email
+    self.email = email.downcase
+  end
 end
