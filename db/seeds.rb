878997dad3f0e9e70ba6c537eb02992281a08835
Robert Nelson <nelson.robert.edward@gmail.com>
2018-06-09 21:31:05 -0500

    client show

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,12 @@
 
 99.times do
   FactoryBot.create(:client, first_name: Faker::Name.first_name,
-                     last_name: Faker::Name.last_name,
-                     email: Faker::Internet.email,
-                     phone: Faker::PhoneNumber.phone_number)
+                    last_name: Faker::Name.last_name,
+                    email: Faker::Internet.email,
+                    phone: Faker::PhoneNumber.phone_number,
+                    address1: Faker::Address.street_address,
+                    address2: Faker::Address.secondary_address,
+                    city: Faker::Address.city,
+                    state: Faker::Address.state,
+                    zip_code: Faker::Address.zip_code)
 end
