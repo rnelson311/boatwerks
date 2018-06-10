d925bedbaadc25a974dfb9db0940318abe89f3ea
Robert Nelson <nelson.robert.edward@gmail.com>
2018-06-10 14:19:15 -0500

    styling and error messages

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -69,6 +69,6 @@ class ClientsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def client_params
-      params.require(:client).permit(:first_name, :last_name, :phone, :email)
+      params.require(:client).permit(:first_name, :last_name, :phone, :email, :address1, :address2, :city, :state, :zip_code)
     end
 end
