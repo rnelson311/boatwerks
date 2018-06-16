d5380c3d9e8dc932263062a6899ad5a2ada36a34
Robert Nelson <nelson.robert.edward@gmail.com>
2018-06-15 20:55:57 -0500

    final marinas

--- spec/views/marinas/index.html.haml_spec.rb
+++ spec/views/marinas/index.html.haml_spec.rb
@@ -1,34 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe "marinas/index", type: :view do
-  before(:each) do
-    assign(:marinas, [
-      Marina.create!(
-        :name => "Name",
-        :address1 => "Address1",
-        :address2 => "Address2",
-        :city => "City",
-        :state => "State",
-        :zip_code => "Zip Code"
-      ),
-      Marina.create!(
-        :name => "Name",
-        :address1 => "Address1",
-        :address2 => "Address2",
-        :city => "City",
-        :state => "State",
-        :zip_code => "Zip Code"
-      )
-    ])
-  end
 
-  it "renders a list of marinas" do
-    render
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => "Address1".to_s, :count => 2
-    assert_select "tr>td", :text => "Address2".to_s, :count => 2
-    assert_select "tr>td", :text => "City".to_s, :count => 2
-    assert_select "tr>td", :text => "State".to_s, :count => 2
-    assert_select "tr>td", :text => "Zip Code".to_s, :count => 2
-  end
 end
